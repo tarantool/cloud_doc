9bceabfb9f15eaac21c5b51babb420c080f5984b
Konstantin Nazarov <mail@racktear.com>
2017-02-07 17:11:20 +0300

    Set visual theme to bootswatch-yeti

--- source/conf.py
+++ source/conf.py
@@ -31,7 +31,7 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = ['sphinxjp.themes.basicstrap']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -83,13 +83,17 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'basicstrap'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+  'inner_theme': True,
+  'inner_theme_name': 'bootswatch-yeti',
+}
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -152,6 +156,3 @@ texinfo_documents = [
      author, 'tarantoolio', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
